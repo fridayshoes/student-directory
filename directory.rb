327ca3237aa5cf234b38a38e1ca9f4b2771d52a8
fridayshoes <one_amp@hotmail.com>
2022-05-11 10:54:11 +0000

    Step 8 / Task 1: Added index to student directory

--- directory.rb
+++ directory.rb
@@ -26,8 +26,8 @@ end
 
 # Print list of students
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
